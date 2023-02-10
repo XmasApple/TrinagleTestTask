d58e97226a8c101c53230b2b21a9a3136c937a23
XmasApple <y4blokof@yandex.ru>
2023-02-11 00:31:03 +0300

    Add FactoryTest

---
+++ Tests/FactoryTest.cs
@@ -0,0 +1,29 @@
+﻿using TrinagleTestTask;
+using TrinagleTestTask.Exceptions;
+using TrinagleTestTask.Shapes;
+
+namespace Tests;
+
+public class FactoryTest
+{
+    [Test]
+    public void Creation()
+    {
+        {
+            var circle = ShapeFactory.Instance.GetShape("Circle", 5);
+            Assert.That(circle, Is.TypeOf<Circle>());
+            Assert.That(circle.GetArea(), Is.EqualTo(25 * Math.PI));
+        }
+        {
+            var triangle = ShapeFactory.Instance.GetShape("Triangle", 3, 4, 5);
+            Assert.That(triangle, Is.TypeOf<Triangle>());
+            Assert.That(triangle.GetArea(), Is.EqualTo(6));
+        }
+    }
+
+    [Test]
+    public void CreationWithWrongShapeName()
+    {
+        Assert.Throws<ShapeNotFoundException>(() => ShapeFactory.Instance.GetShape("WrongShapeName", 1, 2, 3));
+    }
+}
